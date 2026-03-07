e9924bdbccd55752802fc6213ac1630283d6a514
Wawrzyniec Madej <wawrzyniec.madej@gmail.com>
2026-03-07 22:54:15 +0100

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,15 @@
 import AlbumCard from "../components/AlbumCard"
+import { albums } from "../data/albums"
 
 export default function Home() {
 
-    const albums = [
-        {
-            id: "1",
-            title: "LOFI collection",
-            image: "./albums/lofi-collection.jpg",
-            description: "A study of isolation through monochrome textures."
-        },
-        {
-            id: "2",
-            title: "Miniatures Collection",
-            image: "./albums/miniatures-collection.jpg",
-            description: "Urban shadows and abstract geometry."
-        },
-        {
-            id: "3",
-            title: "Szumotron's Saga",
-            image: "./albums/szumotron-saga.jpg",
-            description: "Minimal compositions exploring silence."
-        }
-    ];
-
     return (
             <div className="container mx-auto px-6">
+
+                {albums.map((album) =>
+                    <span>Album {album.title}</span>
+                )}
+
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
 
