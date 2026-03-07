bfca3a23ed33875aafddbc4e6371554ff22ecad4
Wawrzyniec Madej <wawrzyniec.madej@gmail.com>
2026-03-07 22:49:57 +0100

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import AlbumCard from "../components/AlbumCard"
-import { albums } from "../data/albums"
 
 export default function Home() {
 
-    console.log({
-        albums: albums ?? null
-    });
+    const albums = [
+        {
+            id: "1",
+            title: "LOFI collection",
+            image: "./albums/lofi-collection.jpg",
+            description: "A study of isolation through monochrome textures."
+        },
+        {
+            id: "2",
+            title: "Miniatures Collection",
+            image: "./albums/miniatures-collection.jpg",
+            description: "Urban shadows and abstract geometry."
+        },
+        {
+            id: "3",
+            title: "Szumotron's Saga",
+            image: "./albums/szumotron-saga.jpg",
+            description: "Minimal compositions exploring silence."
+        }
+    ];
 
     return (
             <div className="container mx-auto px-6">
