63a2cd3f101c2ad8a188c7b7149af0eafafee7d6
Wawrzyniec Madej <wawrzyniec.madej@gmail.com>
2026-03-08 11:56:22 +0100

    Tracks

--- src/data/albums.js
+++ src/data/albums.js
@@ -3,18 +3,39 @@ export const albums = [
         id: "1",
         title: "LOFI collection",
         image: "./albums/lofi-collection.jpg",
-        description: "A study of isolation through monochrome textures."
+        description: "A study of isolation through monochrome textures.",
+        tracks: [
+            {
+                name: "Something"
+            },
+            {
+                name: "Something 2"
+            },
+            {
+                name: "Something 2"
+            }
+        ]
     },
     {
         id: "2",
         title: "Miniatures Collection",
         image: "./albums/miniatures-collection.jpg",
-        description: "Urban shadows and abstract geometry."
+        description: "Urban shadows and abstract geometry.",
+        tracks: [
+            {
+                name: "Something"
+            }
+        ]
     },
     {
         id: "3",
         title: "Szumotron's Saga",
         image: "./albums/szumotron-saga.jpg",
-        description: "Minimal compositions exploring silence."
+        description: "Minimal compositions exploring silence.",
+        tracks: [
+            {
+                name: "Something"
+            }
+        ]
     }
 ]
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,28 +1,44 @@
-import { useParams } from "react-router-dom"
-import { albums } from "../data/albums"
-import { motion } from "framer-motion"
+import {useParams} from "react-router-dom"
+import {albums} from "../data/albums"
+import {motion} from "framer-motion"
 
 export default function Album() {
 
-    const { id } = useParams()
+    const {id} = useParams()
 
     const album = albums.find(a => a.id === id)
 
     return (
         <main className="max-w-4xl mx-auto">
+            <div className="grid grid-cold-1 md:grid-cols-2 gap-8">
+                <div>
+                    <motion.img
+                        initial={{opacity: 0, scale: 0.95}}
+                        animate={{opacity: 1, scale: 1}}
+                        src={album.image}
+                        className="w-full mb-10"
+                    />
+                </div>
+                <div>
+                    <h1 className="text-xl mb-4">{album.title}</h1>
 
-            <motion.img
-                initial={{ opacity: 0, scale: 0.95 }}
-                animate={{ opacity: 1, scale: 1 }}
-                src={album.image}
-                className="w-full mb-10"
-            />
+                    <p className="text-md text-white/70 leading-relaxed mb-6">
+                        {album.description}
+                    </p>
 
-            <h1 className="text-4xl mb-6">{album.title}</h1>
+                    {album.tracks.length && <>
 
-            <p className="text-lg text-white/70 leading-relaxed">
-                {album.description}
-            </p>
+                        <h3 className="text-md mb-2">Track list</h3>
+                        <ol className="list-decimal list-inside">
+                            {album.tracks.map(track =>
+                                <li className="text-white/70">{track.name}</li>
+                            )}
+                        </ol>
+                    </>}
+
+
+                </div>
+            </div>
 
         </main>
     )
